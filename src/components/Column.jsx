2ca8319896a0abd1b39cbdb35bfe865ea5957210
ihab-boudargham <ihab.boudargham@hotmail.com>
2023-12-27 22:36:27 +0200

    Task

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,5 +1,9 @@
 import './Column.css';
+import Task from './Task.jsx';
 
 export default function Column( {state} ) {
-    return <div className="column"> {state} </div>
+    return <div className="column"> 
+        <p>{state}</p>  
+        <Task title = "Todo"/>
+    </div>
 }
